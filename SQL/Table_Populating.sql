a3cd179bca358a63e04707616b5fba15160d458a
Murilo Barbosa <mbn2@cin.ufpe.br>
2022-10-20 22:34:47 -0300

    Add initial table population
    Co-authored-by: RenatoNogFer <RenatoNogFer@users.noreply.github.com>
    Co-authored-by: Kammerer <victorKammerer@users.noreply.github.com>

---
+++ SQL/Table_Populating.sql
@@ -0,0 +1,238 @@
+INSERT INTO Agente VALUES(
+    'Cypher',
+    'Jaula Cibernetica',
+    'Camera de Vigilancia',
+    'Fio-armadilha',
+    'Assalto Neural',
+    'Sentinela'
+);
+
+INSERT INTO Agente VALUES(
+    'Raze',
+    'Carga de Explosivos',
+    'Cartuchos de Tinta',
+    'Bumba',
+    'Estraga-prazeres',
+    'Duelista'
+);
+
+INSERT INTO Agente VALUES(
+    'Breach',
+    'Estopim',
+    'Falha Tectonica',
+    'Pos-choque',
+    'Onda Trovejante',
+    'Iniciador'
+);
+
+INSERT INTO Agente VALUES(
+    'Killjoy',
+    'Robo de Alarme',
+    'Torreta',
+    'Nanoenxame',
+    'Confinamento',
+    'Sentinela'
+);
+
+INSERT INTO Agente VALUES(
+    'Viper',
+    'Nuvem Venenosa',
+    'Cortina Toxica',
+    'Veneno de Cobra',
+    'Poco Peconhento',
+    'Controlador'
+);
+
+INSERT INTO Agente VALUES(
+    'Neon',
+    'Ricochete Eletrico',
+    'Equipamento Voltaico',
+    'Via Expressa',
+    'Sobrecarga',
+    'Duelista'
+);
+
+INSERT INTO Agente VALUES(
+    'KAY/O',
+    'Granada/Clarao',
+    'Ponto/Zero',
+    'Frag/Mento',
+    'Anular/Cmd',
+    'Iniciador'
+);
+
+INSERT INTO Agente VALUES(
+    'Yoru',
+    'Ponto Cego',
+    'Passagem Dimensional',
+    'Falcatrua',
+    'Espionagem Dimensional',
+    'Duelista'
+);
+
+INSERT INTO Agente VALUES(
+    'Reyna',
+    'Devorar',
+    'Dispensar',
+    'Olhar Voraz',
+    'Imperatriz',
+    'Duelista'
+);
+
+INSERT INTO Arma VALUES(
+    'Classic',
+    'Arma Leve',
+    0
+);
+
+INSERT INTO Arma VALUES(
+    'Vandal',
+    'Rifle',
+    2900
+);
+
+INSERT INTO Arma VALUES(
+    'Bucky',
+    'Escopeta',
+    850
+);
+
+INSERT INTO Arma VALUES(
+    'Phantom',
+    'Rifle',
+    2900
+);
+
+INSERT INTO Arma VALUES(
+    'Marshal',
+    'Sniper',
+    950
+);
+
+INSERT INTO Arma VALUES(
+    'Operator',
+    'Sniper',
+    4700
+);
+
+INSERT INTO Arma VALUES(
+    'Stinger',
+    'SMG',
+    950
+);
+
+INSERT INTO Arma VALUES(
+    'Odin',
+    'Metralhadora',
+    3200
+);
+
+INSERT INTO Arma VALUES(
+    'Faca',
+    'Faca',
+    0
+);
+
+INSERT INTO Jogador VALUES(
+    'Renemoo'
+);
+
+INSERT INTO Jogador VALUES(
+    'Ana Bollizzant'
+);
+
+INSERT INTO Jogador VALUES(
+    'kkmr'
+);
+
+INSERT INTO Jogador VALUES(
+    'Risky Business'
+);
+
+INSERT INTO Jogador VALUES(
+    'Bombacate'
+);
+
+INSERT INTO Jogador VALUES(
+    'KORZIN'
+);
+
+INSERT INTO Jogador VALUES(
+    'Trap Pillow'
+);
+
+INSERT INTO Mapa VALUES(
+    'Haven',
+    3
+);
+
+INSERT INTO Mapa VALUES(
+    'Fracture',
+    2
+);
+
+INSERT INTO Mapa VALUES(
+    'Ascent',
+    2
+);
+
+INSERT INTO Mapa VALUES(
+    'Icebox',
+    2
+);
+
+INSERT INTO Skin VALUES(
+    0,
+    'Classic Glitchpop',
+    2175
+);
+
+INSERT INTO Skin VALUES(
+    1,
+    'Vandal Ancifogo',
+    2475
+);
+
+INSERT INTO Skin VALUES(
+    2,
+    'Phantom Oni',
+    1775
+);
+
+INSERT INTO Skin VALUES(
+    3,
+    'Bucky Crise Radiante 001',
+    1775
+);
+
+INSERT INTO Skin VALUES(
+    4,
+    'Marshal Galeria',
+    875
+);
+
+INSERT INTO Skin VALUES(
+    5,
+    'Operator Ancifogo',
+    2475
+);
+
+INSERT INTO Skin VALUES(
+    6,
+    'Stinger Aritocrata',
+    1275
+
+);
+
+INSERT INTO Skin VALUES(
+    7,
+    'Odin Xenocaçador',
+    1775
+
+);
+
+INSERT INTO Skin VALUES(
+    8,
+    'Confronto Ancifogo',
+    4950
+);
